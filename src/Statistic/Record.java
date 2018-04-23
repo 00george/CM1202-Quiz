6e6bd1d9e35c9842c2c460f3a270418656de7d29
Tommy Cheung <tommy84652@gmail.com>
2018-04-23 20:52:31 +0100

    Score Box Added!

--- src/Statistic/Record.java
+++ src/Statistic/Record.java
@@ -89,28 +89,57 @@ public class Record implements Serializable {
         return mostIncorrectQuestion;
     }
 
-    public Set<String> getUniqueSchoolName() {
+    public ArrayList<String> getUniqueSchoolName() {
         ArrayList<String> uniqueSchoolName = new ArrayList<String>();
 
         for (RecordEntry re: records) {
-            uniqueSchoolName.add(re.getSchoolName());
+            if (!uniqueSchoolName.contains(re.getSchoolName())) {
+                uniqueSchoolName.add(re.getSchoolName());
+            }
         }
 
-        // makes it unique
-        Set<String> setUniqueSchoolName = new HashSet<String>(uniqueSchoolName);
-
-        for (String s: setUniqueSchoolName) {
-            System.out.println(s);
-        }
-
-        return setUniqueSchoolName;
+        return uniqueSchoolName;
     }
 
-    public void leaderboard() {
-        // TODO
-        String[] schoolName;
-        int[] correctQuestionCount;
-        int[] timeTotal;
+    public void scoreBox() {
+        ArrayList<String> uniqueSchoolName = getUniqueSchoolName();
+        ArrayList<Integer> totalCorrectQuestion = new ArrayList<Integer>();
+        ArrayList<Integer> totalTime = new ArrayList<Integer>(uniqueSchoolName.size());
+
+        // init the ArrayList (add 0 to it)
+        for (int i = 0; i < uniqueSchoolName.size(); i++) {
+            totalCorrectQuestion.add(0);
+            totalTime.add(0);
+        }
+
+        // counting corrected question and total time for each school
+        for (int i = 0; i < uniqueSchoolName.size(); i++) {
+            int tmpCorrect = 0;
+            int tmpTime = 0;
+
+            for (RecordEntry re: records) {
+                if (re.getSchoolName().equals(uniqueSchoolName.get(i))) {
+                    tmpCorrect += re.getQuestionCorrect().size();
+                    tmpTime += re.getTime();
+                }
+            }
+            totalCorrectQuestion.set(i, tmpCorrect);
+            totalTime.set(i, tmpTime);
+        }
+
+        String tableFormat = "| %-5d | %-30.30s | %-18d | %-10d |%n";
+
+        System.out.format("+-------+--------------------------------+--------------------+------------+%n");
+        System.out.format("|                               SCORE BOX                                  |%n");
+        System.out.format("+-------+--------------------------------+--------------------+------------+%n");
+        System.out.format("|       | School Name                    | Corrected Question | Total Time |%n");
+        System.out.format("+-------+--------------------------------+--------------------+------------+%n");
+
+        for (int i = 0; i < uniqueSchoolName.size(); i++) {
+            System.out.format(tableFormat, i + 1, uniqueSchoolName.get(i), totalCorrectQuestion.get(i), totalTime.get(i));
+        }
+
+        System.out.format("+-------+--------------------------------+--------------------+------------+%n");
     }
 
     public void save(String filename) {
--- src/Statistic/RecordLoader.java
+++ src/Statistic/RecordLoader.java
@@ -2,25 +2,9 @@ package Statistic;
 
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class RecordLoader {
-    public static void main(String[] args) {
-        String filename = "Records.dat";
-
-        Record r = new Record();
-
-        try {
-            r = load(filename);
-        } catch (Exception e) {
-            // Do nothing
-        }
-
-
-        r.showTable();
-
-
-    }
-
     public static Record load(String filename) throws Exception {
         try {
             ObjectInputStream in = new ObjectInputStream(new FileInputStream("data/" + filename));
