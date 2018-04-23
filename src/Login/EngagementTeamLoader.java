ff011796448b82c9d5c24d32d559b8d0c0ad4346
00george <george.kenny2@googlemail.com>
2018-04-23 20:10:24 +0100

    Fixed files not existing

--- src/Login/EngagementTeamLoader.java
+++ src/Login/EngagementTeamLoader.java
@@ -1,12 +1,33 @@
 package Login;
 
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class EngagementTeamLoader {
 
     public static EngagementTeam load(String filename) throws Exception {
         try {
+
+            Scanner input = new Scanner(System.in);
+            Path folder = Paths.get("data/Engagement.dat");
+            File file = new File("data/Engagement.dat");
+            if(!file.exists()){
+                Files.createDirectories(folder.getParent());
+                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data/Engagement.dat"));
+                out.close();
+                System.out.println("NO LOGIN ACCOUNT");
+                System.out.println("Please enter a new username: ");
+                String username = input.nextLine();
+                System.out.println("Please enter a new password: ");
+                String password = input.nextLine();
+                EngagementTeam et = new EngagementTeam();
+                et.add(username,password);
+                et.save("data/Engagement.dat");
+            }
+
             ObjectInputStream in = new ObjectInputStream(new FileInputStream("data/" + filename));
             EngagementTeam r = (EngagementTeam)in.readObject();
             in.close();
--- src/Statistic/RecordLoader.java
+++ src/Statistic/RecordLoader.java
@@ -1,7 +1,6 @@
 package Statistic;
 
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+import java.io.*;
 
 public class RecordLoader {
     public static void main(String[] args) {
@@ -23,6 +22,12 @@ public class RecordLoader {
 
     public static Record load(String filename) throws Exception {
         try {
+            File file = new File("data/Records.dat");
+            if(!file.exists()){
+                ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("data/Records.dat"));
+                out.writeObject("");
+                out.close();
+            }
             ObjectInputStream in = new ObjectInputStream(new FileInputStream("data/" + filename));
             Record r = (Record)in.readObject();
             in.close();
